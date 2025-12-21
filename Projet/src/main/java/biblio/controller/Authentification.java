c70bc93cc0099f4533ccb1959f482c0c23d0f08e
malak594 <melghazi799@gmail.com>
2025-12-21 18:39:48 +0100

    Delete Projet directory

--- Projet/src/main/java/biblio/controller/Authentification.java
+++
@@ -1,8 +0,0 @@
-package biblio.controller;
-
-public class Authentification {
-    public boolean login(String nom, String pw) {
-
-        return false;
-    }
-}
--- Projet/src/main/java/biblio/data/DataBase.java
+++
@@ -1,9 +0,0 @@
-package biblio.data;
-import java.sql.Connection;
-
-public class DataBase {
-    public static Connection getConnection() {
-
-        return null;
-    }
-}
